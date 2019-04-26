08c060ce2769b494151df8a17a1b5ddeff946eb7
James Sadler <freshtonic@gmail.com>
2019-04-26 11:31:09 +1000

    Draw arena as SVG rectangle
    SVG coordinate system has been set up to be the same as the game arena.
    
    The SVG will be remndered as a square with a height the same as the
    viewport height.

--- lib/blast_web/live/game_live.ex
+++ lib/blast_web/live/game_live.ex
@@ -3,9 +3,9 @@ defmodule BlastWeb.GameLive do
 
   def render(assigns) do
     ~L"""
-    <div>
-      GAME
-    </div>
+    <svg class="arena" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
+      <rect x="0" y="0" width="1000" height="1000" />
+    </svg>
     """
   end
 
