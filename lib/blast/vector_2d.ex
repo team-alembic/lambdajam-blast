9821dc787594a97fb8b8252446824e953bce4d57
James Sadler <freshtonic@gmail.com>
2019-05-14 13:19:49 +1000

    Add type specs to Vector2D

--- lib/blast/vector_2d.ex
+++ lib/blast/vector_2d.ex
@@ -7,24 +7,33 @@ defmodule Blast.Vector2D do
   alias Blast.Vector2D
 
   typedstruct enforce: true do
-    field(:x, float())
-    field(:y, float())
+    field :x, float()
+    field :y, float()
   end
 
   @degrees_per_radian 57.2958
 
+  @spec north() :: Blast.Vector2D.t()
   def north, do: unit(new(0, -1))
+
+  @spec east() :: Blast.Vector2D.t()
   def east, do: unit(new(1, 0))
+
+  @spec south() :: Blast.Vector2D.t()
   def south, do: unit(new(1, 1))
+
+  @spec west() :: Blast.Vector2D.t()
   def west, do: unit(new(-1, 0))
 
-  def new(x, y) do
+  @spec new(number(), number()) :: Blast.Vector2D.t()
+  def new(x, y) when is_number(x) and is_number(y) do
     %Vector2D{x: x, y: y}
   end
 
   @doc """
   Return the magnitude (length) of the vector.
   """
+  @spec mag(Blast.Vector2D.t()) :: number()
   def mag(%Vector2D{x: x, y: y}) do
     :math.sqrt(x * x + y * y)
   end
@@ -32,6 +41,7 @@ defmodule Blast.Vector2D do
   @doc """
   Return a vector of unit length with direction preserved.
   """
+  @spec unit(Blast.Vector2D.t()) :: Blast.Vector2D.t()
   def unit(v = %Vector2D{x: x, y: y}) do
     len = mag(v)
 
@@ -45,10 +55,12 @@ defmodule Blast.Vector2D do
   @doc """
   Add two vectors together.
   """
+  @spec add(Blast.Vector2D.t(), Blast.Vector2D.t()) :: Blast.Vector2D.t()
   def add(%Vector2D{x: x1, y: y1}, %Vector2D{x: x2, y: y2}) do
     %Vector2D{x: x1 + x2, y: y1 + y2}
   end
 
+  @spec add([Blast.Vector2D.t(), ...]) :: Blast.Vector2D.t()
   def add([]), do: raise("expected a non-empty list")
   def add([v = %Vector2D{}]), do: v
 
@@ -59,10 +71,12 @@ defmodule Blast.Vector2D do
   @doc """
   Subtract one vector from another.
   """
+  @spec sub(Blast.Vector2D.t(), Blast.Vector2D.t()) :: Blast.Vector2D.t()
   def sub(%Vector2D{x: x1, y: y1}, %Vector2D{x: x2, y: y2}) do
     %Vector2D{x: x1 - x2, y: y1 - y2}
   end
 
+  @spec sub([Blast.Vector2D.t(), ...]) :: Blast.Vector2D.t()
   def sub([]), do: raise("expected a non-empty list")
   def sub([v = %Vector2D{}]), do: v
 
@@ -73,6 +87,7 @@ defmodule Blast.Vector2D do
   @doc """
   Multiply the magnitude of the vector by a multiplier.
   """
+  @spec multiply_mag(Blast.Vector2D.t(), number()) :: Blast.Vector2D.t()
   def multiply_mag(%Vector2D{x: x, y: y}, multiplier) do
     %Vector2D{x: x * multiplier, y: y * multiplier}
   end
@@ -80,6 +95,7 @@ defmodule Blast.Vector2D do
   @doc """
   Computes the dot product of two Vector2D
   """
+  @spec dot(Blast.Vector2D.t(), Blast.Vector2D.t()) :: number()
   def dot(%Vector2D{x: x1, y: y1}, %Vector2D{x: x2, y: y2}) do
     x1 * x2 + y1 * y2
   end
@@ -87,6 +103,7 @@ defmodule Blast.Vector2D do
   @doc """
   Computes the cross product of two Vector2D
   """
+  @spec cross(Blast.Vector2D.t(), Blast.Vector2D.t()) :: number()
   def cross(%Vector2D{x: x1, y: y1}, %Vector2D{x: x2, y: y2}) do
     x1 * y2 - x2 * y1
   end
@@ -96,6 +113,7 @@ defmodule Blast.Vector2D do
 
   Output is -180 to +180.
   """
+  @spec signed_angle_between(Blast.Vector2D.t(), Blast.Vector2D.t()) :: number()
   def signed_angle_between(v1 = %Vector2D{}, v2 = %Vector2D{}) do
     :math.atan2(cross(v1, v2), dot(v1, v2)) * @degrees_per_radian
   end
@@ -103,6 +121,7 @@ defmodule Blast.Vector2D do
   @doc """
   Negate the `x` component of the vector.
   """
+  @spec invert_x(Blast.Vector2D.t()) :: Blast.Vector2D.t()
   def invert_x(%Vector2D{x: x, y: y}) do
     %Vector2D{x: -x, y: y}
   end
@@ -110,6 +129,7 @@ defmodule Blast.Vector2D do
   @doc """
   Negate the `y` component of the vector.
   """
+  @spec invert_y(Blast.Vector2D.t()) :: Blast.Vector2D.t()
   def invert_y(%Vector2D{x: x, y: y}) do
     %Vector2D{x: x, y: -y}
   end
@@ -119,6 +139,7 @@ defmodule Blast.Vector2D do
 
   +ve degrees are right, -ve are left
   """
+  @spec rotate(Blast.Vector2D.t(), number()) :: Blast.Vector2D.t()
   def rotate(%Vector2D{x: x, y: y}, degrees) do
     radians = degrees * (:math.pi() / 180)
 
@@ -131,6 +152,7 @@ defmodule Blast.Vector2D do
   @doc """
   Return the distance between two vectors using Pythagoras Theorem.
   """
+  @spec distance_between(Blast.Vector2D.t(), Blast.Vector2D.t()) :: number()
   def distance_between(%Vector2D{x: x1, y: y1}, %Vector2D{x: x2, y: y2}) do
     :math.sqrt(
       :math.pow(abs(x1 - x2), 2) +
@@ -138,10 +160,12 @@ defmodule Blast.Vector2D do
     )
   end
 
-  def random(x_range, y_range) do
-    new(Enum.random(x_range), Enum.random(y_range))
+  @spec random(Range.t(), Range.t()) :: Blast.Vector2D.t()
+  def random(min_x..max_x, min_y..max_y) do
+    new(Enum.random(min_x..max_x), Enum.random(min_y..max_y))
   end
 
+  @spec unit_random() :: Blast.Vector2D.t()
   def unit_random() do
     random(-10000..10000, -10000..10000) |> unit()
   end
