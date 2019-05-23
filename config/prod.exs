00e50db02df4f9e42bbf9dda73acc3a4be6f3f44
James Sadler <freshtonic@gmail.com>
2019-05-23 13:42:26 +1000

    Force SSL in prod

--- config/prod.exs
+++ config/prod.exs
@@ -12,6 +12,7 @@ use Mix.Config
 config :blast, BlastWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [host: "blast.alembic.com.au", port: 80],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
