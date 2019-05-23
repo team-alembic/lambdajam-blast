e51fade9231c4fa9440f48f0bebb993e789cb872
James Sadler <freshtonic@gmail.com>
2019-05-23 14:20:30 +1000

    Use truly random game IDs
    Prevents people from predicting the next ID and joining your game.

--- lib/blast/game_launch_server.ex
+++ lib/blast/game_launch_server.ex
@@ -12,34 +12,32 @@ defmodule Blast.GameLaunchServer do
 
   alias Blast.GameServer
 
-  @counter_seed 0
-
   def start_link([]) do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
   def init([]) do
-    {:ok, @counter_seed}
+    {:ok, nil}
   end
 
-  def handle_call({:new}, _from, counter) do
-    {:ok, game_id} = launch_game_server(counter)
-    {:reply, {:ok, game_id}, counter + 1}
+  def handle_call({:new}, _from, _) do
+    {:ok, game_id} = launch_game_server()
+    {:reply, {:ok, game_id}, nil}
   end
 
   def new() do
     GenServer.call(__MODULE__, {:new})
   end
 
-  defp launch_game_server(counter) do
-    game_id = generate_token(counter)
+  defp launch_game_server() do
+    game_id = generate_token()
     name = {:via, Registry, {GameServerRegistry, game_id}}
     {:ok, _} = GameServer.start_link(name: name)
     {:ok, game_id}
   end
 
-  defp generate_token(counter) do
-    :crypto.hash(:md5, Integer.to_string(counter))
+  defp generate_token() do
+    :crypto.hash(:md5, Integer.to_string(Enum.random(0..999_999_999_999_999)))
     |> Base.encode16()
     |> String.slice(1..4)
   end
